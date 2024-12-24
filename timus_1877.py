da23f6649a1a9de2e38eafe082281535d627940e
Eklas-Mohin <mohin030299@gmail.com>
2024-12-25 00:29:08 +0600

    Timus 1877. Bicycle Codes

---
+++ timus_1877.py
@@ -0,0 +1,11 @@
+def main():
+    n = int(input())
+    m = int(input())
+
+    if n % 2 == 1 and m % 2 == 0:
+        print('no')
+    else:
+        print('yes')
+
+if __name__ == '__main__':
+    main()
